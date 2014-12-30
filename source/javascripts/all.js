aca8f1bfc7bd04daa713a8d982314fb97c53f5ed
Chris Neitzer <chris@neitzer.com>
2014-12-30 16:52:53 -0500

    Center and responsify slideshow
    Also center controls and responsify

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,29 +3,3 @@
 //= require lodash
 //= require bean
 //= require revolver
-
-var mySlider = new Revolver({
-  containerSelector: '.slideshow',
-  slidesSelector: '.slide'
-});
-
-$(".pause, .play").click(function(e){
-  e.preventDefault();
-
-  var $icon = $(this).find('i');
-
-  if ($icon.hasClass('icon-pause')) {
-    $icon.attr('class', 'icon-play-3');
-    mySlider.pause();
-  } else {
-    $icon.attr('class', 'icon-pause');
-    mySlider.play();
-  }
-});
-
-$(".prev, .next").click(function(e){
-  e.preventDefault();
-
-  var action = $(this).attr('class') === 'prev' ? "previous" : "next";
-  mySlider[action]();
-});
---
+++ source/javascripts/app.js.coffee
@@ -0,0 +1,25 @@
+(($) ->
+  slider_options =
+    containerSelector: '.slideshow',
+    slidesSelector: '.slide'
+
+  mySlider = new Revolver slider_options
+
+  $(".pause, .play").click (e) ->
+    e.preventDefault()
+
+    $icon = $(@).find('i')
+
+    if $icon.hasClass('icon-pause')
+      $icon.attr('class', 'icon-play-3')
+      mySlider.pause()
+    else
+      $icon.attr('class', 'icon-pause')
+      mySlider.play()
+
+  $(".prev, .next").click (e) ->
+    e.preventDefault()
+
+    action = if $(this).attr('class') is 'prev' then "previous" else "next"
+    mySlider[action]()
+) jQuery
